c526ca971e8f10d55f18bc41a579fd969f1245fa
OnyeEbube <okontaonyeka@gmail.com>
2023-06-27 20:27:33 +0000

    Adding corrections to file 101

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,10 +1,11 @@
 export default function iterateThroughObject(reportWithIterator) {
   let output = '';
+  const keys = Object.keys(reportWithIterator);
 
-  for (const [index, item] of Object.entries(reportWithIterator)) {
-    output += `${item}`;
+  for (const [index, key] of keys.entries()) {
+    output += reportWithIterator[key];
 
-    if (parseInt(index) !== reportWithIterator.length - 1) {
+    if (index !== keys.length - 1) {
       output += ' | ';
     }
   }
