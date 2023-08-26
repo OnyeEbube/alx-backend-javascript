db4a22e5373b76396efbfb2bcabdd67ebb168c19
OnyeEbube <okontaonyeka@gmail.com>
2023-08-26 04:34:55 -0700

    Adding files

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,8 +1,15 @@
-console.log("Welcome to Holberton School, what is your name?")
-process.stdin.setEncoding('utf8');
-process.stdin.on('data', (data) => {
-	console.log(`Your name is: ${data}`);
-});
-process.stdin.on('end', () => {
-	console.log('This important software is now closing\n');
-});
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+if (process.stdin.isTTY) {
+  process.stdin.on('data', (data) => {
+    process.stdout.write(`Your name is: ${data.toString()}`);
+    process.exit();
+  });
+} else {
+  process.stdin.on('data', (data) => {
+    process.stdout.write(`Your name is: ${data.toString()}`);
+    process.exit();
+  });
+  process.on('exit', () => {
+    process.stdout.write('This important software is now closing\n');
+  });
+}
