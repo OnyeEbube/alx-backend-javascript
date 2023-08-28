bf7d9186f8990b83950a4dd384950b8bd807bafd
OnyeEbube <okontaonyeka@gmail.com>
2023-08-28 06:13:43 -0700

    Adding the solutions

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,22 +1,22 @@
-const fs = require('fs')
+const fs = require('fs');
 
-function countStudents(path) {
+function countStudents(fileName) {
   const students = {};
   const fields = {};
   let length = 0;
   try {
-    const content = fs.readFileSync(path, 'utf-8');
+    const content = fs.readFileSync(fileName, 'utf-8');
     const lines = content.toString().split('\n');
     for (let i = 0; i < lines.length; i += 1) {
       if (lines[i]) {
         length += 1;
         const field = lines[i].toString().split(',');
         if (Object.prototype.hasOwnProperty.call(students, field[3])) {
-	  students[field[3]].push(field[0]);
-	} else {
-	  students[field[3]] = [field[0]];
-	}
-	if (Object.prototype.hasOwnProperty.call(fields, field[3])) {
+          students[field[3]].push(field[0]);
+        } else {
+          students[field[3]] = [field[0]];
+        }
+        if (Object.prototype.hasOwnProperty.call(fields, field[3])) {
           fields[field[3]] += 1;
         } else {
           fields[field[3]] = 1;
